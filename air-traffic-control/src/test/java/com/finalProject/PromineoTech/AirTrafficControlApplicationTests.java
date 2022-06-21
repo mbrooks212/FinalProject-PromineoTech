aa32b902f9538f4fd38c625d0eb031ebe3c423ef
Michael <michaelbrooks212@gmail.com>
2022-06-21 16:43:39 -0400

    Add files via upload

---
+++ air-traffic-control/src/test/java/com/finalProject/PromineoTech/AirTrafficControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.PromineoTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTrafficControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
