7de770373a7ea20485f50854298cc5c272dd63bf
Cooper Johnson <cooperjohnson523@gmail.com>
2022-06-22 12:01:18 -0700

    done?

--- air-traffic-control/src/main/java/com/finalProject/PromineoTech/controller/FuelRequiredController.java
+++ air-traffic-control/src/main/java/com/finalProject/PromineoTech/controller/FuelRequiredController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.finalProject.PromineoTech.model.FuelRequired;
-import com.finalProject.PromineoTech.model.TypeOfFlight;
 import com.finalProject.PromineoTech.service.FuelRequiredService;
 
 /**
