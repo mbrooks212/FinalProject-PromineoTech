1f339cdbe901fa6680dbb88e8fdf3fac0646e0ec
Cooper Johnson <99623408+CooperALJohnson@users.noreply.github.com>
2022-06-22 11:44:49 -0700

    Add files via upload

---
+++ air_traffic_control_fuel_required_and_type_of_flight.sql
@@ -0,0 +1,21 @@
+-- fuel_required
+
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (1, 1, 63500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (2, 2, 43500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (3, 3, 43500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (4, 4, 43500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (5, 5, 4500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (6, 6, 43500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (7, 7, 63500);
+INSERT INTO fuel_required (id, model, amount_of_fuel) VALUES (8, 8, 63500);
+
+-- type_of_flight
+
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (1, 1, 1, 1);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (2, 1, 1, 2);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (3, 2, 1, 3);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (4, 1, 1, 4);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (5, 1, 2, 5);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (6, 1, 2, 6);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (7, 1, 1, 7);
+INSERT INTO type_of_flight (id, airport, airline, flight) VALUES (8, 1, 1, 8);
